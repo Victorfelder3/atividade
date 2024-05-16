a4b5a44c940a3c289beb06ae38054218283ed933
Victorfelder3 <167728780+Victorfelder3@users.noreply.github.com>
2024-05-16 17:21:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function calcularSoma() {
+    let numero1 = parseFloat(document.getElementById('numero1').value);
+    let numero2 = parseFloat(document.getElementById('numero2').value);
+    let resultado = numero1 + numero2;
+    document.getElementById('resultado').textContent = 'O Resultado é: ' + resultado;
+  }
